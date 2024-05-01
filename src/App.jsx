8940bfb76d9a6ce74ad386a6099d6b3046503881
Antonio-Stefan Coman <coman.d.antoniostefan22@stud.rau.ro>
2024-05-01 14:25:08 +0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import SingIn from "./pages/SingIn"
 import SingUp from "./pages/SingUp"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 export default function App() {
   return <BrowserRouter>
+  <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sing-in" element={<SingIn />} />
