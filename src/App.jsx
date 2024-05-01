54d3f70674f06a99be6734673852af0326ffe744
Antonio-Stefan Coman <coman.d.antoniostefan22@stud.rau.ro>
2024-05-01 13:58:50 +0300

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SingIn from "./pages/SingIn"
+import SingUp from "./pages/SingUp"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
 
 export default function App() {
-  return (
-    <h1>App</h1>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sing-in" element={<SingIn />} />
+      <Route path="/sing-up" element={<SingUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
 }
