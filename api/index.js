7a4d90a4374ae134d6cdff69df92c2ca4c0281e3
Antonio-Stefan Coman <coman.d.antoniostefan22@stud.rau.ro>
2024-05-01 14:50:58 +0300

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log('Error:', error);
+});
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
-    });
+    }
+);
