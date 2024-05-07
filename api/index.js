6fd130a5e594ec466bd258cceeef89da90c0e26a
Antonio-Stefan Coman <coman.d.antoniostefan22@stud.rau.ro>
2024-05-07 22:18:49 +0300

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
@@ -23,6 +25,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
